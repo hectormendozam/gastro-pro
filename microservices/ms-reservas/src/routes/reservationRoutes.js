18abaf510225a3f825820630fda002bf92fd5312
Jos :) <joselynrali705@gmail.com>
2026-05-14 20:24:33 -0600

    (feat): Controlador y rutas REST para ms-reservas

---
+++ microservices/ms-reservas/src/routes/reservationRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { createReservation, getReservations } = require('../controllers/reservationController');
+
+const router = express.Router();
+
+router.post('/', createReservation);
+router.get('/', getReservations);
+
+module.exports = router;
